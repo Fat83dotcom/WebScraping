676359847d4ec33d87e83891a4fbe13e873234ac
fat83dotcom <fernando.quimic@gmail.com>
2022-10-13 12:56:21 -0300

    capturando o texto em um arquivo txt

--- web_sacraping.py
+++ web_sacraping.py
@@ -1,18 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.cnnbrasil.com.br/politica/'
-resposta = requests.get(url)
-html = BeautifulSoup(resposta.text, 'html.parser')
+with open('noticias.txt', '+a') as arquivo:
+    url = 'https://www.cnnbrasil.com.br/politica/'
+    resposta = requests.get(url)
+    html = BeautifulSoup(resposta.text, 'html.parser')
 
-for links in html.select('.home__list__item'):
-    tituloMateria = links.a.get_text()
-    linkMateria = links.a.get('href')
-    print(tituloMateria)
-    print(linkMateria)
-    resp = requests.get(linkMateria)
-    html1 = BeautifulSoup(resp.text, 'html.parser')
-    for texto in html1.select('.post__content'):
-        texto = texto.p.get_text()
-        print(texto, '\n')
-
+    for links in html.select('.home__list__item'):
+        tituloMateria = links.a.get_text()
+        linkMateria = links.a.get('href')
+        arquivo.write(tituloMateria)
+        arquivo.write(linkMateria)
+        print(tituloMateria)
+        print(linkMateria)
+        resp = requests.get(linkMateria)
+        html1 = BeautifulSoup(resp.text, 'html.parser')
+        for texto in html1.select('.post__content'):
+            texto = texto.get_text()
+            arquivo.write(texto)
+            print(texto, '\n')
