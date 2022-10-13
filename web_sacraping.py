a3ed429784114a2b3a4bf726de48e625e41348df
fat83dotcom <fernando.quimic@gmail.com>
2022-10-12 21:28:04 -0300

    Script para capturar os textos de todas as materias sobre politicado portal CNN

---
+++ web_sacraping.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.cnnbrasil.com.br/politica/'
+resposta = requests.get(url)
+html = BeautifulSoup(resposta.text, 'html.parser')
+
+for links in html.select('.home__list__item'):
+    tituloMateria = links.a.get_text()
+    linkMateria = links.a.get('href')
+    print(tituloMateria)
+    print(linkMateria)
+    resp = requests.get(linkMateria)
+    html1 = BeautifulSoup(resp.text, 'html.parser')
+    for texto in html1.select('.post__content'):
+        texto = texto.get_text()
+        print(texto)
