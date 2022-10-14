5e6230c1f0db9aa66615917efe6dd7f4ce6c4ac3
fat83dotcom <fernando.quimic@gmail.com>
2022-10-14 18:58:27 -0300

    Formatações de codigo e mudanção an atualização do template html

--- web_sacraping.py
+++ web_sacraping.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from itertools import count
 
-with open(f'noticiasCNN{datetime.now().strftime("%d-%m-%Y-%H-%M-%S")}.html', 'a+') as arquivo:
+with open(f'noticiasCNN.html', 'w') as arquivo:
     url = [
         'https://www.cnnbrasil.com.br/politica/',
         'https://www.cnnbrasil.com.br/nacional/',
@@ -28,26 +28,44 @@ with open(f'noticiasCNN{datetime.now().strftime("%d-%m-%Y-%H-%M-%S")}.html', 'a+
     <link rel="stylesheet" href="style.css"> 
 </head>
 <body>
+    
     '''
     )
     for links in url:
         resposta = requests.get(links)
         html = BeautifulSoup(resposta.text, 'html.parser')
-        arquivo.write(f'<h1>Seção do Site:</h1> <a href="{links}" target="_blanck">{links}</a> ')
+        arquivo.write(
+            '''
+    <div>
+        <a href="#https://www.cnnbrasil.com.br/politica/">Política</a>
+        <a href="#https://www.cnnbrasil.com.br/nacional/">Nacional</a>
+        <a href="#https://www.cnnbrasil.com.br/business/">Business</a>
+        <a href="#https://www.cnnbrasil.com.br/internacional/">Internacional</a>
+        <a href="#https://www.cnnbrasil.com.br/esporte/">Esporte</a>
+        <a href="#https://www.cnnbrasil.com.br/saude/">Saude</a>
+        <a href="#https://www.cnnbrasil.com.br/tecnologia/">Tecnologia</a>
+        <a href="#https://www.cnnbrasil.com.br/entretenimento/">Entretenimento</a>
+        <a href="#https://www.cnnbrasil.com.br/estilo/">Estilo</a>
+        <a href="#https://www.cnnbrasil.com.br/loterias/">Loterias</a>
+    </div>
+            '''
+        )
+        arquivo.write(f'<div class="titulo"><h1 id="{links}">Seção do Site:</h1> <a href="{links}" target="_blanck">'
+        f'{links}</a> </div>')
         contador = count(1)
         for noticias in html.select('.home__list__item'):
             tituloMateria = noticias.a.get_text()
             linkMateria = noticias.a.get('href')
-            arquivo.write(f'<h2>{next(contador)} Horario do Scraping: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}</h2> '
+            arquivo.write(f'<div class="titulo"> <h2>Nº {next(contador)} | Horario do Scraping: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}'
+            f' </h2> '
             f'<h3>{tituloMateria}</h3> <a href="{linkMateria}" target="_blanck">Link Materia</a> ')
-            print(tituloMateria)
-            print(linkMateria)
             resp = requests.get(linkMateria)
             html1 = BeautifulSoup(resp.text, 'html.parser')
-            for texto in html1.select('.post__content'):
-                texto = texto.get_text(" | ", strip=True)
+            for materia in html1.select('.posts'):
+                dataMateria = materia.select_one('.post__data').get_text(strip=True)
+                texto = materia.select_one('.post__content').get_text(' | ', strip=True)
+                arquivo.write(f'<h3>Data Matéria: {dataMateria}</h3> </div>')
                 arquivo.write(f'<div> <p>{texto}</p> </div>')
-                print(texto, '\n')
     arquivo.write(
         '''
 </body>
